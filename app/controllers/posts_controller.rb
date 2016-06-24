bfa7dd18e529c2c4b79afb7886befb167ec4b8a2
Brian Simmons <bsimmonsn@gmail.com>
2016-06-24 15:45:26 -0500

    fixed commenting issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
     @post = Post.find_by id: params[:id]
     @comment = Comment.new
     @comment.body = params[:comment][:body]
+    @comment.author = params[:comment][:author]
     @comment.post_id = @post.id
     if @comment.save
       redirect_to post_path(id: @post.id)
